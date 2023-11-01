74be21e4d98bcca596560bf2c68c0b94386b1f6e
dinesh lokhande <dineshlokhande702@gmail.com>
2023-11-01 14:23:39 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <br></br>
+        <br></br>
+<div>
+        <img src="image1.jpg" className="App-logo" alt="logo" />
+        <img src="image2.jpg" className="App-logo" alt="logo" />
+        <img src="image5.jpg" className="App-logo" alt="logo" />
+
+
+
+        </div>
+        <div>
+        <img src="image3.jpg" className="App-logo" alt="logo" />
+        <img src="image4.jpg" className="App-logo" alt="logo" />
+        <img src="imgae.jpg" className="App-logo" alt="logo" />
+
+
+
+        </div>
+        <br></br>
+        <br></br>
+        <h1>
+          welcome to Image the gallary 
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +36,25 @@ function App() {
         >
           Learn React
         </a>
+        <br></br>
+        <br></br>
+        <div>
+        <img src="image1.jpg" className="App-logo" alt="logo" />
+        <img src="image2.jpg" className="App-logo" alt="logo" />
+        <img src="image5.jpg" className="App-logo" alt="logo" />
+
+
+
+        </div>
+        <div>
+        <img src="image3.jpg" className="App-logo" alt="logo" />
+        <img src="image4.jpg" className="App-logo" alt="logo" />
+        <img src="imgae.jpg" className="App-logo" alt="logo" />
+
+
+        <br></br>
+        <br></br>
+        </div>
       </header>
     </div>
   );
